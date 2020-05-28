6daf0ab186015f93bd8d926aa7ce0ecdbb8ea653
SiriK404 <sirishakotikalapudi94@gmail.com>
2020-05-29 04:11:14 +0530

    Update handleopener.py

--- handleopener.py
+++ handleopener.py
@@ -1,26 +1,25 @@
 ##Opening handle to a specific  process with pid 
-
 import ctypes
+k_handle = ctypes.WinDLL("Kernel32.dll")
 
-k_handle=ctypes.WinDLL("Kernel32.dll")
+PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xfff)
 
-PROCESS_ALL_ACCESS= ( 0x000F0000 | 0x00100000 | 0xfff)
+dwDesiredAccess = PROCESS_ALL_ACCESS
+bInheritHandle = False
 
-dwDesiredAccess=PROCESS_ALL_ACCESS
-bInheritHandle= False
+ProcessId= int(input("Process Id:"))
 
-ProcessId=int(input("Enter the process id of the process to create a handle to :"))
+response = k_handle.OpenProcess(dwDesiredAccess, bInheritHandle,ctypes.c_ulong(ProcessId))
 
-response=k_handle.OpenProcess(dwDesiredAccess,bInheritHandle,ProcessId)
+error = k_handle.GetLastError()
 
-error=k_handle.GetLastError()
-
-if error !=0 :
+if error != 0:
     print("error code is : {0}".format(error))
-    #exit(1)
-    
-if response <=0:
+    # exit(1)
+
+if response <= 0:
     print("handle not created")
 
 else:
     print(response)
+
